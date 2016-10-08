57a69aa33b7105e47ddb5fa8973aaa38a36a030b
ori yampolsky <orleg01@gmail.com>
2016-10-08 20:43:05 +0300

    The Whole Project
    created for youtube tutorial

---
+++ Realistic Water/Assets/WaterProgram/Scripts/SeaMesh.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshFilter  ))]
+[RequireComponent(typeof(MeshRenderer))]
+public class SeaMesh : MonoBehaviour {
+
+    public int resX = 200;
+    public int resY = 200;
+
+	void Start ()
+    {
+        Mesh mesh = new Mesh();
+
+        mesh.vertices  = getDotes();
+        mesh.uv        = getUv();
+        mesh.triangles = getIndexes();
+        mesh.normals   = getNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+	}
+
+    private Vector3[] getDotes()
+    {
+        List<Vector3> dotesToReturn = new List<Vector3>();
+
+        for (int i = 0; i < resX; i++)
+            for (int j = 0; j < resY; j++)
+                dotesToReturn.Add(new Vector3(i , 0 , j));
+
+        return dotesToReturn.ToArray();
+    }
+
+    private Vector2[] getUv()
+    {
+        List<Vector2> uvToReturn = new List<Vector2>();
+
+        for (int i = 0; i < resX; i++)
+            for (int j = 0; j < resY; j++)
+                uvToReturn.Add(new Vector2((float)i / (resX - 1) , (float) j / (resY - 1)));
+
+        return uvToReturn.ToArray();
+
+    }
+
+    private int[] getIndexes()
+    {
+        List<int> indexToReturn = new List<int>();
+
+        for (int i = 0; i < resX - 1; i++)
+        {
+            for (int j = 0; j < resY - 1; j++)
+            {
+                indexToReturn.Add(i + (j * resX));
+                indexToReturn.Add(i + (j * resX) + 1);
+                indexToReturn.Add(i + ((j + 1) * resX));
+                indexToReturn.Add(i + (j * resX) + 1);
+                indexToReturn.Add(i + ((j + 1) * resX) + 1);
+                indexToReturn.Add(i + ((j + 1) * resX));
+            }
+        }
+
+        return indexToReturn.ToArray();
+    }
+
+    private Vector3[] getNormals()
+    {
+        Vector3[] normals = new Vector3[resX * resY];
+
+        for (int i = 0; i < normals.Length; i++)
+            normals[i] = new Vector3(0, 1, 0);
+
+        return normals;
+    }
+}
---
+++ Realistic Water/Assets/WaterProgram/Shaders/SeaShader.shader
@@ -0,0 +1,89 @@
+﻿Shader "Unlit/SeaShader"
+{
+
+	Properties
+	{
+		_heightImageOne("height1" , 2D) = "White"{}
+		_heightImageTwo("height2" , 2D) = "White"{}
+		_height("maxHeight" , FLOAT) = 5
+		_scale("scale" , FLOAT) = 2
+	}
+
+
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+				float3 normals : TEXCOORD1;
+				float3 worldPos : TEXCOORD2;
+			};
+
+			sampler2D _heightImageOne;
+			sampler2D _heightImageTwo;
+			float _height;
+			float _scale ;
+			
+			float getMyHeight(float2 cord)
+			{
+				float flow = _Time;
+				return (tex2Dlod(_heightImageOne, float4(float2( flow,  flow) + cord, 0, 0)).y * _height) +
+					   (tex2Dlod(_heightImageTwo, float4(float2(-flow, -flow) + cord, 0, 0)).y * _height);
+			}
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				float myHeight, leftHeight, forwardHeight;
+
+				v.uv *= _scale;
+				myHeight = v.vertex.y = getMyHeight(v.uv.xy);
+
+				leftHeight    = getMyHeight(float2(v.uv.x + 0.005f, v.uv.y)) - myHeight;
+				forwardHeight = getMyHeight(float2(v.uv.x, v.uv.y + 0.005f)) - myHeight;
+				o.normals = normalize(float3(-leftHeight, 1, -forwardHeight));
+
+				o.worldPos = mul(unity_ObjectToWorld, v.vertex).xyz;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+
+				return o;
+			}
+			
+
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = 0 ;
+				
+				half3 worldViewDir = normalize(UnityWorldSpaceViewDir(i.worldPos));
+				half3 worldReflect = reflect(-worldViewDir, normalize(i.normals));
+				half4 skyData = UNITY_SAMPLE_TEXCUBE(unity_SpecCube0, worldReflect);
+				half3 skyColor = DecodeHDR(skyData, unity_SpecCube0_HDR);
+
+				col.rgb = skyColor;
+
+				return col;
+			}
+
+			ENDCG
+		}
+	}
+}
